153bd762499d86c950c75b0c4459dfcab4aa28fc
chung <chungchung.dev@gmail.com>
2024-01-03 00:38:11 +0800

    add validation for the Android db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import database
+from jsonschema import validate
 
 if __name__ == '__main__':
 	os.chdir("..")
@@ -10,7 +11,11 @@ if __name__ == '__main__':
 		db_json = database.get_remote_hkbusetadb_text()
 		an_db = database.generate_db_for_android(db_json)
 		pretty_an_db = json.dumps(an_db, indent=4, ensure_ascii=False)
+		print(an_db)
+		with open('schema/AndroidRouteFareListSchema.json', 'r', encoding='utf-8') as schema_file:
+			an_db_schema = json.load(schema_file)
+			validate(an_db, an_db_schema)
 		with open('data/AndroidRouteFareList.json', 'w', encoding='utf-8') as f:
 			f.writelines(pretty_an_db)
 	except (TypeError, ValueError, KeyError) as err:
-		print(err, "in generating json for Android.")
+		print(type(err), err)
