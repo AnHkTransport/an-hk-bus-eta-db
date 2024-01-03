e518f9d5cd21433cee6b9066aa2c06954f09a8eb
chung <chungchung.dev@gmail.com>
2024-01-03 10:03:02 +0800

    add 7zip compression for the Android db

--- src/main.py
+++ src/main.py
@@ -2,20 +2,25 @@ import json
 import os
 import database
 from jsonschema import validate
+import py7zr
 
 if __name__ == '__main__':
-	os.chdir("..")
-	# etadb_path = Path('test/EtaDbSample.json')
-	# etadb_path = Path('data/routeFareList.json')
-	try:
-		db_json = database.get_remote_hkbusetadb_text()
-		an_db = database.generate_db_for_android(db_json)
-		pretty_an_db = json.dumps(an_db, indent=4, ensure_ascii=False)
-		print(an_db)
-		with open('schema/AndroidRouteFareListSchema.json', 'r', encoding='utf-8') as schema_file:
-			an_db_schema = json.load(schema_file)
-			validate(an_db, an_db_schema)
-		with open('data/AndroidRouteFareList.json', 'w', encoding='utf-8') as f:
-			f.writelines(pretty_an_db)
-	except (TypeError, ValueError, KeyError) as err:
-		print(type(err), err)
+    os.chdir('..')
+    try:
+        db_json = database.get_remote_hkbusetadb_text()
+        an_db = database.generate_db_for_android(db_json)
+        pretty_an_db = json.dumps(an_db, indent=4, ensure_ascii=False)
+
+        with open('schema/AndroidRouteFareListSchema.json', 'r', encoding='utf-8') as schema_file:
+            an_db_schema = json.load(schema_file)
+            validate(an_db, an_db_schema)
+
+        with open('data/AndroidRouteFareList.json', 'w', encoding='utf-8') as f:
+            f.writelines(pretty_an_db)
+
+        os.chdir('data')
+        with py7zr.SevenZipFile('AndroidRouteFareList.7z', 'w') as archive:
+            archive.write('AndroidRouteFareList.json')
+
+    except (TypeError, ValueError, KeyError) as err:
+        print(type(err), err)
